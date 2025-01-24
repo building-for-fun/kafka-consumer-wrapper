8957a1cebb88d6d99c2fb1f11e7cd2e464fb5dc1
Akshit Batra <akshit.batra@josys.com>
2025-01-25 01:59:48 +0530

    initial commit

---
+++ src/KafkaConsumer.ts
@@ -0,0 +1,54 @@
+import { Kafka, Consumer, EachMessagePayload } from 'kafkajs';
+
+export interface KafkaConsumerConfig {
+  brokers: string[];
+  groupId: string;
+  topic: string;
+  fromBeginning?: boolean;
+  handleMessage: (message: any) => Promise<void>;
+}
+
+export class KafkaConsumerWrapper {
+  private kafka: Kafka;
+  private consumer: Consumer;
+  private topic: string;
+  private handleMessage: (message: any) => Promise<void>;
+
+  constructor(config: KafkaConsumerConfig) {
+    this.kafka = new Kafka({ brokers: config.brokers });
+    this.consumer = this.kafka.consumer({ groupId: config.groupId });
+    this.topic = config.topic;
+    this.handleMessage = config.handleMessage;
+  }
+
+  async connect(): Promise<void> {
+    await this.consumer.connect();
+    console.log(`Connected to Kafka as group: ${this.consumer.groupId}`);
+  }
+
+  async subscribe(fromBeginning = false): Promise<void> {
+    await this.consumer.subscribe({ topic: this.topic, fromBeginning });
+    console.log(`Subscribed to topic: ${this.topic}`);
+  }
+
+  async run(): Promise<void> {
+    await this.consumer.run({
+      eachMessage: async ({ topic, partition, message }: EachMessagePayload) => {
+        try {
+          const value = message.value?.toString();
+          if (value) {
+            await this.handleMessage(JSON.parse(value));
+          }
+        } catch (err) {
+          console.error(`Error processing message: ${err}`);
+        }
+      },
+    });
+    console.log(`Consumer is running on topic: ${this.topic}`);
+  }
+
+  async disconnect(): Promise<void> {
+    await this.consumer.disconnect();
+    console.log('Consumer disconnected.');
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { KafkaConsumerWrapper, KafkaConsumerConfig } from './KafkaConsumer';
